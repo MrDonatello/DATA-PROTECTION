23a9642349141158812db5cf610e39016d5437a0
Andrey <meet123666@gmail.com>
2020-06-02 18:28:12 +0600

    modification

--- Caesar cipher/src/sample/Main.java
+++ Caesar cipher/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Caesar cipher");
-        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setScene(new Scene(root, 750, 450));
         primaryStage.show();
     }
 
